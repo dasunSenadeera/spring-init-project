471049019a459dd1101ba361f93656110f4242ac
chathuran <chathuran@codegen.net>
2020-01-23 17:54:52 +0530

    init commit

---
+++ src/main/java/com/example/angularspringbootserver/AngularSpringbootServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/angularspringbootserver/AngularSpringbootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringbootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
